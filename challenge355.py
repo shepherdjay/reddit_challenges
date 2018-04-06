532227297056eb4536d8dc01dcd6a7b4569221a2
shepherdjay <shepherdjay@users.noreply.github.com>
2018-04-06 16:04:20 -0600

    Initial Commit

---
+++ challenge355.py
@@ -0,0 +1,94 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/87rz8c/20180328_challenge_355_intermediate_possible/
+
+# Recipes
+PUMPKIN = {
+    "pumpkin": 1,
+    "apple": 0,
+    "eggs": 3,
+    "milk": 4,
+    "sugar": 3
+}
+
+APPLE = {
+    "pumpkin": 0,
+    "apple": 1,
+    "eggs": 4,
+    "milk": 3,
+    "sugar": 2
+}
+
+
+def map_ingredients(ingredients_as_string: str):
+    """ Returns dictionary of ingredients from input string """
+    pumpkin, apple, eggs, milk, sugar = [int(x) for x in ingredients_as_string.split(',')]
+    return {
+        "pumpkin": pumpkin,
+        "apple": apple,
+        'eggs': eggs,
+        'milk': milk,
+        'sugar': sugar
+    }
+
+
+def make_pie(ingredients_dict, recipe_dict):
+    """
+    Bakes a Pie
+    :param ingredients_dict: Dictionary of current ingredients on hand
+    :param recipe_dict: Dictionary of recipe
+    :return: Tuple of (bool, dict) whether the pie was baked, dictionary of remaining ingredients
+    """
+    remaining_ingredients = {}
+    for ingredient, amount in recipe_dict.items():
+        try:
+            remaining_ingredients[ingredient] = ingredients_dict[ingredient] - amount
+            if remaining_ingredients[ingredient] < 0:
+                return False, ingredients_dict
+        except KeyError:
+            return False, ingredients_dict
+    return True, remaining_ingredients
+
+
+def iterate_pies_of_type(ingredients_dict, type, pies_to_bake):
+    """
+    Takes an ingredient dict, type of pie, and the amount of pies to bake
+    Returns amount of pies actually baked and remaining ingredients
+    """
+    if type == 'pumpkin':
+        recipe = PUMPKIN
+    elif type == 'apple':
+        recipe = APPLE
+    else:
+        raise ValueError('Pie Type Not Known')
+
+    remaining_ingredients = ingredients_dict
+
+    pies_baked = 0
+    while pies_baked != pies_to_bake:
+        # Try to make x pies of that type then the rest apple and add to solution array
+        result, remaining_ingredients = make_pie(remaining_ingredients, recipe)
+        if result:
+            pies_baked += 1
+        else:
+            break
+
+    return pies_baked, remaining_ingredients
+
+
+def make_pies(ingredients_as_string):
+    ingredients_dict = map_ingredients(ingredients_as_string)
+    # solution_array is tuples of (pumpkin, apple)
+    solution_array = []
+
+    for x in range(ingredients_dict['pumpkin']):
+        # On each loop try to make that many pumpkin and then make the rest apple
+        # Update solution space
+        pumpkin_pies_baked, pumpkin_rem_ingredients = iterate_pies_of_type(ingredients_dict, 'pumpkin', x)
+        for y in range(pumpkin_rem_ingredients['apple']):
+            apple_pies_baked, final_rem_ingredients = iterate_pies_of_type(pumpkin_rem_ingredients, 'apple', y)
+            solution_array.append((pumpkin_pies_baked, apple_pies_baked))
+
+    sum_pies = [sol[0] + sol[1] for sol in solution_array]
+
+    solution_index = sum_pies.index(max(sum_pies))
+
+    return solution_array[solution_index]
---
+++ test_challenge355.py
@@ -0,0 +1,53 @@
+from challenge355 import make_pies, map_ingredients, make_pie, PUMPKIN, APPLE, iterate_pies_of_type
+import pytest
+
+INPUT1 = '10,14,10,42,24'
+INPUT2 = '12,4,40,30,40'
+INPUT3 = '12,14,20,42,24'
+
+INGREDIENTS_DICT_1 = {'pumpkin': 10, 'apple': 14, 'eggs': 10, 'milk': 42, 'sugar': 24}
+INGREDIENTS_DICT_2 = {'pumpkin': 12, 'apple': 4, 'eggs': 40, 'milk': 30, 'sugar': 40}
+INGREDIENTS_DICT_3 = {'pumpkin': 12, 'apple': 14, 'eggs': 20, 'milk': 42, 'sugar': 24}
+
+
+@pytest.mark.parametrize(
+    'ingredients,exp_pumpkin,exp_apple', [
+        (INPUT1, 3, 0),
+        (INPUT2, 5, 3),
+        (INPUT3, 5, 1)
+    ])
+def test_make_pies(ingredients, exp_pumpkin, exp_apple):
+    act_pumpkin, act_apple = make_pies(ingredients)
+
+    assert exp_pumpkin + exp_apple == act_pumpkin + act_apple
+
+
+@pytest.mark.parametrize(
+    'ingredients_string,exp_dictionary', [
+        (INPUT1, INGREDIENTS_DICT_1),
+        (INPUT2, INGREDIENTS_DICT_2),
+        (INPUT3, INGREDIENTS_DICT_3)
+    ])
+def test_map_ingredients(ingredients_string, exp_dictionary):
+    assert exp_dictionary == map_ingredients(ingredients_string)
+
+
+@pytest.mark.parametrize(
+    'ingredients_dict,recipe,exp_result,exp_remaining', [
+        (INGREDIENTS_DICT_1, PUMPKIN, True, {'pumpkin': 9, 'apple': 14, 'eggs': 7, 'milk': 38, 'sugar': 21}),
+        (INGREDIENTS_DICT_3, APPLE, True, {'pumpkin': 12, 'apple': 13, 'eggs': 16, 'milk': 39, 'sugar': 22}),
+        ({'pumpkin': 1}, APPLE, False, {'pumpkin': 1})
+    ]
+)
+def test_make_pie(ingredients_dict, recipe, exp_result, exp_remaining):
+    act_result, act_remaining_ingredients = make_pie(ingredients_dict, recipe)
+
+    assert exp_result == act_result
+    assert exp_remaining == act_remaining_ingredients
+
+
+def test_iterate_pies_of_type():
+    exp_pies_baked, exp_remaining_ingredients = 3, {'pumpkin': 7, 'apple': 14, 'eggs': 1, 'milk': 30, 'sugar': 15}
+    act_pies_baked, act_remaining_ingredients = iterate_pies_of_type(INGREDIENTS_DICT_1, 'pumpkin', 4)
+
+    assert exp_remaining_ingredients == act_remaining_ingredients
