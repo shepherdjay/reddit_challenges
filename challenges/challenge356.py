a168acace90b534d6694c593a649315ee311f32d
Jay Shepherd <shepherdjay@users.noreply.github.com>
2018-04-13 19:01:40 -0600

    Removed random component of challenge356
    Also removed sorted as the new method guarantees the list will be from
    smallest to largest.

--- challenges/challenge356.py
+++ challenges/challenge356.py
@@ -1,23 +1,22 @@
 # /r/dailyprogrammer/comments/8bh8dh/20180411_challenge_356_intermediate_goldbachs/
+from itertools import product
 from typing import List
+
 from sympy import primerange
-import random
 
 
 def find_goldbach(n: int) -> List:
     odd_primes = [prime for prime in primerange(0, n) if prime % 2 != 0]
 
-    solution_found = False
-    while not solution_found:
-        solution = random.choices(odd_primes, k=3)
-        if sum(solution) == n:
-            solution_found = True
+    solution_space = product(odd_primes[:], odd_primes[:], odd_primes[:])
 
-    return solution
+    for possible_solution in solution_space:
+        if sum(possible_solution) == n:
+            return possible_solution
 
 
 if __name__ == '__main__':
     challenge_inputs = [111, 17, 199, 287, 53]
     for number in challenge_inputs:
-        solution = sorted(find_goldbach(number))
+        solution = find_goldbach(number)
         print(f'{number} = {solution[0]} + {solution[1]} + {solution[2]}')
