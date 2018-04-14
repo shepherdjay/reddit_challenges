1d99e0c22e1af1facdac223949aead98510f6120
Jay Shepherd <shepherdjay@users.noreply.github.com>
2018-04-13 19:08:54 -0600

    Introduce shuffling just to make the output more interesting
    Added an extra input when running as __main__ for 1,000,001

--- challenges/challenge356.py
+++ challenges/challenge356.py
@@ -1,13 +1,13 @@
 # /r/dailyprogrammer/comments/8bh8dh/20180411_challenge_356_intermediate_goldbachs/
 from itertools import product
 from typing import List
-
+from random import shuffle
 from sympy import primerange
 
 
 def find_goldbach(n: int) -> List:
     odd_primes = [prime for prime in primerange(0, n) if prime % 2 != 0]
-
+    shuffle(odd_primes)
     solution_space = product(odd_primes[:], odd_primes[:], odd_primes[:])
 
     for possible_solution in solution_space:
@@ -18,5 +18,5 @@ def find_goldbach(n: int) -> List:
 if __name__ == '__main__':
     challenge_inputs = [111, 17, 199, 287, 53]
     for number in challenge_inputs:
-        solution = find_goldbach(number)
+        solution = sorted(find_goldbach(number))
         print(f'{number} = {solution[0]} + {solution[1]} + {solution[2]}')
