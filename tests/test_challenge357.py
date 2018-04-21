005758d633e6596c6e05014ed22ce32909050a52
Jay Shepherd <shepherdjay@users.noreply.github.com>
2018-04-21 17:32:41 -0600

    Add skipif statement for Travis builds
    Travis will timeout after 10 minutes of no output. We can expand that
    to 50 but instead we will just skip any computationally heavy tests.
    Not sure power of the Travis environment, one of the tests takes
    1 minute on my machine but over 10 on Travis

--- tests/test_challenge357.py
+++ tests/test_challenge357.py
@@ -1,11 +1,12 @@
 import pytest
+import os
 
 from challenges.challenge357_int import kolokaski_ratio, nillson_generator
 
 KOLOKASKI = '12211212212211211221211212211'
 
+skip_travis = pytest.mark.skipif(os.environ.get('TRAVIS') == 'true', reason="Test too slow to run on Travis")
 
-@pytest.mark.known
 @pytest.mark.parametrize(
     'input, exp_output', [
         (10, (5, 5)),
@@ -20,14 +21,15 @@ def test_kolokaski_expected(input, exp_output):
 @pytest.mark.parametrize(
     'input', [
         pytest.param(1000000, marks=pytest.mark.challenge),
-        pytest.param(100000000, marks=pytest.mark.challenge),
-        pytest.param(1000000000000, marks=pytest.mark.bonus),
-        pytest.param(100000000000000, marks=pytest.mark.bonus)
+        pytest.param(100000000, marks=[pytest.mark.challenge, skip_travis]), # This isn't slow on local processor but is on travis
+        pytest.param(1000000000000, marks=[pytest.mark.bonus, pytest.mark.slowtest, skip_travis]),
+        pytest.param(100000000000000, marks=[pytest.mark.bonus, pytest.mark.slowtest, skip_travis])
     ]
 )
 def test_kolokaski_unknown(input):
     output = kolokaski_ratio(input)
     assert pytest.approx(1, rel=1.0e-04) == (output[0] / output[1])
+    print(output)
 
 
 @pytest.mark.parametrize(
